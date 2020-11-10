7c24a868490cd79278d51706c3a7977a6338b550
William-Moreno <intrcptr@comcast.net>
2020-11-10 13:21:35 -0800

    created functions to create array of labels and votes and to create a chart with those values

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ var imageElements = [leftImageElement, centerImageElement, rightImageElement];
 var randomIndexes = [];
 var votingRounds = 0;
 var maxRounds = 25;
+var productLabels = [];
+var productVotes = [];
 
 function Product(fileName) {
   this.filePath = `img/${fileName}`;
@@ -151,6 +153,84 @@ function displayResults() {
 
   document.getElementById("button-container").innerHTML = "";
   thankYouMessage();
+  generateChart();
+}
+
+function createProductLabels(){
+  for(var i = 0 ; i < allProducts.length ; i++){
+    productLabels.push(allProducts[i].title);
+  }
+}
+
+function createProductVotes(){
+  for(var i = 0 ; i < allProducts.length ; i++){
+    productVotes.push(allProducts[i].votes);
+  }
+}
+
+function generateChart(){
+  createProductLabels();
+  createProductVotes();
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: productLabels,
+          datasets: [{
+              label: '# of Votes',
+              data: productVotes,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)', 'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)', 'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)', 'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
 }
 
 function thankYouMessage() {
@@ -171,3 +251,4 @@ generateRandomIndexes();
 renderImages();
 
 votingElement.addEventListener("click", recordVote);
+
